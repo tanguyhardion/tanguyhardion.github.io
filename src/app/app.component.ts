3f1cdaa16a643e9bdc58d13de1ab66eed539ee95
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2025-01-29 21:03:45 +0100

    add meta config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { StorageHelper } from '@utils/storage-helper';
@@ -15,7 +16,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 export class AppComponent {
   homePage: boolean;
 
-  constructor(private router: Router, private translate: TranslateService) {
+  constructor(
+    private router: Router,
+    private translate: TranslateService,
+    private meta: Meta,
+    private title: Title
+  ) {
     // translation configuration
     const langs = ['fr', 'en'];
     this.translate.addLangs(langs);
@@ -35,6 +41,18 @@ export class AppComponent {
         document.title = this.getTitle(event.url);
       }
     });
+
+    // meta configuration
+    this.title.setTitle('Tanguy Hardion');
+    this.meta.addTags([
+      { name: 'author', content: "Tanguy Hardion's website" },
+      {
+        name: 'description',
+        content:
+          'Student specializing in AI and Data Science at UTC, France. Passionate about technology, innovation and entrepreneurship.'
+      },
+      { name: 'keywords', content: 'Tanguy Hardion, personal website, portfolio' }
+    ]);
   }
 
   private getTitle(route: string) {
