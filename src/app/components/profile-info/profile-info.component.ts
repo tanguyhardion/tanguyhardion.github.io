6c6e38920719c9a83a22ae8487141e8db6138be1
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2025-02-13 22:40:31 +0100

    refactor profile-info component to streamline age display and update styles

--- src/app/components/profile-info/profile-info.component.ts
+++ src/app/components/profile-info/profile-info.component.ts
@@ -1,29 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import dayjs from 'dayjs';
 import { AnimationOptions } from 'ngx-lottie';
 import { LottieComponent } from 'ngx-lottie';
-import { MarkedPipe } from "@pipes/marked.pipe";
+import { MarkedPipe } from '@pipes/marked.pipe';
 
 @Component({
   selector: 'ProfileInfo',
   standalone: true,
   imports: [CommonModule, LottieComponent, TranslatePipe, MarkedPipe],
   templateUrl: './profile-info.component.html',
-  styleUrl: './profile-info.component.scss'
+  styleUrls: ['./profile-info.component.scss']
 })
-export class ProfileInfoComponent implements OnInit {
-  age = '';
+export class ProfileInfoComponent implements AfterViewInit {
+  @ViewChild('info') infoEl!: ElementRef<HTMLDivElement>;
+
   options: AnimationOptions = {
     path: 'assets/lottie/animation.json',
     renderer: 'canvas' as any
   };
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     setInterval(() => {
       const result = dayjs().diff(dayjs(1047423600000), 'year', true);
-      this.age = result.toString().substring(0, 12);
+      const age = result.toString().substring(0, 12);
+      const ageEl = this.infoEl.nativeElement.querySelector('.age-placeholder');
+      if (ageEl) {
+        ageEl.textContent = age;
+      }
     }, 50);
+
+    const links = this.infoEl.nativeElement.getElementsByTagName('a');
+    Array.from(links).forEach((link) => {
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
+    });
   }
 }
