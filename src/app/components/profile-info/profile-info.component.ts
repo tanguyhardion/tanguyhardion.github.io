a2a138b901b9b9a1fa1057e8914e80477ec04cf0
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2025-06-14 23:52:11 +0200

    feat: enhance profile animation

--- src/app/components/profile-info/profile-info.component.ts
+++ src/app/components/profile-info/profile-info.component.ts
@@ -3,21 +3,15 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MarkedPipe } from '@pipes/marked.pipe';
 import dayjs from 'dayjs';
-import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'ProfileInfo',
-  imports: [CommonModule, LottieComponent, TranslatePipe, MarkedPipe],
+  imports: [CommonModule, TranslatePipe, MarkedPipe],
   templateUrl: './profile-info.component.html',
   styleUrls: ['./profile-info.component.scss']
 })
 export class ProfileInfoComponent implements AfterViewInit {
   @ViewChild('info') infoEl!: ElementRef<HTMLDivElement>;
-
-  options: AnimationOptions = {
-    path: 'assets/lottie/animation.json',
-    renderer: 'canvas' as any
-  };
 
   ngAfterViewInit(): void {
     setInterval(() => {
