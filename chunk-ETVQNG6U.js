ad6671054057510c999048f6e658b6d937d9a4f5
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2025-08-27 18:55:16 +0200

    Auto-generated commit

--- chunk-ETVQNG6U.js
+++ chunk-ETVQNG6U.js
@@ -1,11 +1,21 @@
 import{a as p}from"./chunk-QNZTRIIV.js";import{Fa as g,Oa as a,Ta as r,db as h,dc as n,f as s,kc as b,mb as d}from"./chunk-S3JNK5LH.js";var m=class l{constructor(o){this.translateService=o;this.render(),this.translateService.onLangChange.subscribe(()=>{this.render()})}content;skills=`
+  ## {{ 'skills.gen_agentic_ai' }}
+
+  ![LangChain](https://img.shields.io/badge/LangChain-%231d3c3e.svg?style=for-the-badge&logo=LangChain&logoColor=white)
+  ![LangGraph](https://img.shields.io/badge/LangGraph-%231C3C3C.svg?style=for-the-badge&logo=LangGraph&logoColor=white)
+  ![OpenAI](https://img.shields.io/badge/OpenAI-%23412991.svg?style=for-the-badge&logo=OpenAI&logoColor=white)
+  ![Mistral AI](https://img.shields.io/badge/Mistral%20AI-%23FA520F.svg?style=for-the-badge&logo=mistralai&logoColor=white)
+  ![Hugging Face](https://img.shields.io/badge/Hugging%20Face-%23FFD21E.svg?style=for-the-badge&logo=huggingface&logoColor=%23333)
+  ![RAG](https://img.shields.io/badge/RAG-%2300A1EA.svg?style=for-the-badge&logo=Milvus&logoColor=white)
+  ![Prompt Engineering](https://img.shields.io/badge/Prompt%20Engineering-%236B5B95.svg?style=for-the-badge&logo=chatgpt&logoColor=white)
+
   ## {{ 'skills.data_science_ml' }}
 
   ![TensorFlow](https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&logo=TensorFlow&logoColor=white)
   ![Keras](https://img.shields.io/badge/Keras-%23D00000.svg?style=for-the-badge&logo=Keras&logoColor=white)
   ![PyTorch](https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=for-the-badge&logo=PyTorch&logoColor=white)
   ![scikit-learn](https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white)
-  ![SciPy](https://img.shields.io/badge/SciPy-%230C55A5.svg?style=for-the-badge&logo=scipy&logoColor=%white)
+  ![SciPy](https://img.shields.io/badge/SciPy-%230C55A5.svg?style=for-the-badge&logo=scipy&logoColor=white)
   ![NumPy](https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white)
   ![pandas](https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white)
   ![Matplotlib](https://img.shields.io/badge/Matplotlib-%23ffffff.svg?style=for-the-badge&logo=Matplotlib&logoColor=black)
@@ -16,17 +26,25 @@ import{a as p}from"./chunk-QNZTRIIV.js";import{Fa as g,Oa as a,Ta as r,db as h,d
   ![Extract, Transform, Load (ETL)](https://img.shields.io/badge/ETL-%23F37626.svg?style=for-the-badge&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABbElEQVQ4jZ2Sv0vDUBiGn)
   ![Pentaho Kettle](https://img.shields.io/badge/Pentaho%20Kettle-135fa0.svg?style=for-the-badge&logo=Pentaho&logoColor=white)
 
+  ## {{ 'skills.data_analytics' }}
+
+  ![Excel](https://img.shields.io/badge/Microsoft%20Excel-217346?style=for-the-badge&logo=microsoft-excel&logoColor=white)
+  ![Power BI](https://img.shields.io/badge/Power%20BI-F2C811?style=for-the-badge&logo=power-bi&logoColor=black)
+  ![Tableau](https://img.shields.io/badge/Tableau-E97627?style=for-the-badge&logo=tableau&logoColor=white)
+
+
   ## {{ 'skills.programming_languages' }}
 
-  ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
-  ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
-  ![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)
   ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
   ![R](https://img.shields.io/badge/r-%23276DC3.svg?style=for-the-badge&logo=r&logoColor=white)
-  ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white)
+  ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
+  ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+  ![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)
+  ![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)
+  ![C#](https://img.shields.io/badge/c%23-%239843af.svg?style=for-the-badge&logo=sharp&logoColor=white)
   ![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white)
   ![C](https://img.shields.io/badge/C-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white)
-  ![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)
+  ![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white)
 
   ## {{ 'skills.web_development' }}
 
@@ -34,54 +52,60 @@ import{a as p}from"./chunk-QNZTRIIV.js";import{Fa as g,Oa as a,Ta as r,db as h,d
 
   ![Angular](https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white)
   ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
-  ![Next JS](https://img.shields.io/badge/Next-101010?style=for-the-badge&logo=next.js&logoColor=white)
+  ![Next.js](https://img.shields.io/badge/Next.js-101010?style=for-the-badge&logo=next.js&logoColor=white)
   ![Vue.js](https://img.shields.io/badge/vue.js-%2335495e.svg?style=for-the-badge&logo=vuedotjs&logoColor=%234FC08D)
-  ![Nuxt.js](https://img.shields.io/badge/Nuxt.js-%2302ad66.svg?style=for-the-badge&logo=nuxt.js&logoColor=white)
-  ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
-  ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
-  ![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
-  ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
-  ![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white)
-
+  ![Nuxt](https://img.shields.io/badge/Nuxt-%2302ad66.svg?style=for-the-badge&logo=nuxt&logoColor=white)
+  ![Svelte](https://img.shields.io/badge/svelte-%23FF3E00.svg?style=for-the-badge&logo=svelte&logoColor=white)
+  ![Tailwind](https://img.shields.io/badge/Tailwind-%2306B6D4.svg?style=for-the-badge&logo=tailwindcss&logoColor=white)
+  
   ### {{ 'skills.back_end' }}
-
+  
   ![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white)
   ![Flask](https://img.shields.io/badge/flask-%23101010.svg?style=for-the-badge&logo=flask&logoColor=white)
-
+  ![FastAPI](https://img.shields.io/badge/FastAPI-%23009688.svg?style=for-the-badge&logo=fastapi&logoColor=white)
+  ![Vercel](https://img.shields.io/badge/vercel-%23101010.svg?style=for-the-badge&logo=vercel&logoColor=white)
+  ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+  
   ## {{ 'skills.databases' }}
 
   ![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)
+  ![Supabase](https://img.shields.io/badge/Supabase-%2333ab75.svg?style=for-the-badge&logo=supabase&logoColor=white)
+  ![Firebase](https://img.shields.io/badge/Firebase-%23DD2C00.svg?style=for-the-badge&logo=firebase&logoColor=white)
   ![MySQL](https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&logo=mysql&logoColor=white)
   ![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white)
   ![Oracle](https://img.shields.io/badge/Oracle-F80000?style=for-the-badge&logo=oracle&logoColor=white)
   ![SQLite](https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&logo=sqlite&logoColor=white)
 
+  ## {{ 'skills.cloud' }}
+
+  ![Microsoft Azure](https://img.shields.io/badge/Microsoft%20Azure-0078D4?style=for-the-badge&logo=microsoft-azure&logoColor=white)
+
   ## {{ 'skills.frameworks_libraries' }}
 
-  ![Ionic](https://img.shields.io/badge/Ionic-%233880FF.svg?style=for-the-badge&logo=Ionic&logoColor=white)
+  ![Flutter](https://img.shields.io/badge/flutter-%2302569B.svg?style=for-the-badge&logo=flutter&logoColor=white)
   ![.Net](https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white)
   ![Chart.js](https://img.shields.io/badge/chart.js-F5788D.svg?style=for-the-badge&logo=chart.js&logoColor=white)
   ![RxJS](https://img.shields.io/badge/rxjs-%23B7178C.svg?style=for-the-badge&logo=reactivex&logoColor=white)
 
   ## {{ 'skills.devops_tools' }}
 
+  ![CI/CD](https://img.shields.io/badge/CI%2FCD-%232088FF.svg?style=for-the-badge&logo=github-actions&logoColor=white)
   ![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
   ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
-  ![GitHub Pages](https://img.shields.io/badge/github%20pages-121013?style=for-the-badge&logo=github&logoColor=white)
   ![GitLab](https://img.shields.io/badge/gitlab-%23db4128.svg?style=for-the-badge&logo=gitlab&logoColor=white)
   ![Jenkins](https://img.shields.io/badge/jenkins-%232C5263.svg?style=for-the-badge&logo=jenkins&logoColor=white)
   ![Postman](https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white)
   ![Apache Maven](https://img.shields.io/badge/Apache%20Maven-C71A36?style=for-the-badge&logo=Apache%20Maven&logoColor=white)
-  ![NPM](https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&logo=npm&logoColor=white)
-  ![JWT](https://img.shields.io/badge/JWT-101010?style=for-the-badge&logo=JSON%20web%20tokens)
 
   ## {{ 'skills.design_tools' }}
 
   ![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white)
+  ![Balsamiq](https://img.shields.io/badge/Balsamiq-%23404040.svg?style=for-the-badge&logo=Balsamiq&logoColor=white)
   ![Canva](https://img.shields.io/badge/Canva-%232297d4.svg?style=for-the-badge&logo=Canva&logoColor=white)
 
   ## {{ 'skills.project_management' }}
 
   ![Trello](https://img.shields.io/badge/Trello-%23026AA7.svg?style=for-the-badge&logo=Trello&logoColor=white)
   ![Notion](https://img.shields.io/badge/Notion-%23101010.svg?style=for-the-badge&logo=notion&logoColor=white)
+  ![Azure DevOps](https://img.shields.io/badge/Azure%20DevOps-0078D4?style=for-the-badge&logo=microsoft-azure&logoColor=white)
   `;render(){return s(this,null,function*(){let o=yield this.translateContent(this.skills);this.content=yield p(o)})}translateContent(o){return s(this,null,function*(){let e=/\{\{\s*'skills\.(.*?)'\s*\}\}/g,t=[...o.matchAll(e)];for(let i of t){let f=i[1],c=yield this.translateService.get(`skills.${f}`).toPromise();o=o.replace(i[0],c)}return o})}static \u0275fac=function(e){return new(e||l)(a(b))};static \u0275cmp=r({type:l,selectors:[["app-skills"]],decls:1,vars:1,consts:[[1,"skills","rendered-html","markdown-body",3,"innerHTML"]],template:function(e,t){e&1&&d(0,"div",0),e&2&&h("innerHTML",t.content,g)},dependencies:[n],styles:[".skills[_ngcontent-%COMP%]{display:flex;flex-wrap:wrap;flex-direction:column;align-items:center;opacity:0;animation:fadeInUp 1s ease-out .3s forwards}  .skills.rendered-html.markdown-body img{border-radius:8px;-webkit-user-select:none;user-select:none;transition:all .3s ease}  .skills.rendered-html.markdown-body img:hover{transform:scale(1.05);box-shadow:0 8px 20px #0000004d}  .skills.rendered-html.markdown-body h2,   .skills.rendered-html.markdown-body h3{text-align:center;opacity:0;animation:fadeInUp .8s ease-out forwards}  .skills.rendered-html.markdown-body h2{margin-top:32px;margin-bottom:16px;background:linear-gradient(135deg,#fff,#ccc);-webkit-background-clip:text;-webkit-text-fill-color:transparent;background-clip:text}  .skills.rendered-html.markdown-body h3{margin-bottom:16px}  .skills.rendered-html.markdown-body h3:not(:first-of-type){margin-top:24px}  .skills.rendered-html.markdown-body p{text-align:center;line-height:1.5;opacity:0;animation:fadeIn 1s ease-out .5s forwards}"]})};export{m as SkillsComponent};
